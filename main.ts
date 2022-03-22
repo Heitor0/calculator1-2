68b855d86f0c30c651daca0e6febb154fe2dfdb6
Heitor0 <101339934+Heitor0@users.noreply.github.com>
2022-03-22 06:46:06 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,40 @@
+let Var1 = 0
+let Var2 = 0
+let Var3 = 0
+input.onButtonPressed(Button.A, function () {
+    Var1 += 1
+    basic.showNumber(Var1)
+})
+input.onButtonPressed(Button.AB, function () {
+    if (true) {
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # # # .
+            . . # . .
+            . . . . .
+            `)
+        basic.showNumber(Var1 + Var2)
+        Var3 = Var1 + Var2
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    Var2 += -1
+    basic.showNumber(Var2)
+})
+input.onGesture(Gesture.Shake, function () {
+    if (true) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # # .
+            . . . . .
+            . . . . .
+            `)
+        basic.showNumber(Var1 - Var2)
+        Var3 = Var1 - Var2
+    }
+})
+basic.forever(function () {
+	
+})
