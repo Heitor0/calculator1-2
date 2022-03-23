5ecc68fc53008582a61febe603909273534b9daa
Heitor0 <101339934+Heitor0@users.noreply.github.com>
2022-03-23 06:56:01 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,40 +1,85 @@
-let Var1 = 0
-let Var2 = 0
-let Var3 = 0
 input.onButtonPressed(Button.A, function () {
+    // change var by 1 when A is clicked
+    // 
     Var1 += 1
     basic.showNumber(Var1)
 })
 input.onButtonPressed(Button.AB, function () {
-    if (true) {
-        basic.showLeds(`
-            . . . . .
-            . . # . .
-            . # # # .
-            . . # . .
-            . . . . .
-            `)
-        basic.showNumber(Var1 + Var2)
-        Var3 = Var1 + Var2
-    }
+    Var2 = Var1
+    Var1 = 0
 })
 input.onButtonPressed(Button.B, function () {
-    Var2 += -1
-    basic.showNumber(Var2)
+    // changing var by 1
+    Var1 += -1
+    basic.showNumber(Var1)
 })
 input.onGesture(Gesture.Shake, function () {
-    if (true) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . # # # .
-            . . . . .
-            . . . . .
-            `)
-        basic.showNumber(Var1 - Var2)
-        Var3 = Var1 - Var2
-    }
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showNumber(Var1 + Var2)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # # .
+        . . . . .
+        . . . . .
+        `)
+    basic.showNumber(Var1 - Var2)
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showNumber(5)
+    basic.showNumber(4)
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        # # # # #
+        # # # # #
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
     control.reset()
 })
+/**
+ * setting the nums to 1
+ */
+let Var2 = 0
+let Var1 = 0
+Var1 = 0
+Var2 = 0
